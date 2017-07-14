1b98395d88399a22e5a85f913c78f100ac00d532
AnaghSharma <anaghsharma12@gmail.com>
2017-07-14 22:14:59 +0530

    Added helper class for event monitoring

---
+++ Carol/Helpers/EventMonitor.cs
@@ -0,0 +1,41 @@
+﻿using AppKit;
+using Foundation;
+namespace Carol.Helpers
+{
+    public class EventMonitor
+    {
+        NSObject monitor;
+        NSEventMask mask;
+        GlobalEventHandler handler;
+
+        public EventMonitor()
+        {
+            
+        }
+
+        public EventMonitor(NSEventMask mask, GlobalEventHandler handler)
+        {
+            this.mask = mask;
+            this.handler = handler;
+        }
+
+        ~EventMonitor()
+        {
+            Stop();
+        }
+
+        public void Start()
+        {
+            monitor = NSEvent.AddGlobalMonitorForEventsMatchingMask(mask, handler) as NSObject;
+        }
+
+        public void Stop()
+        {
+            if (monitor != null)
+            {
+                NSEvent.RemoveMonitor(monitor);
+                monitor = null;
+            }
+        }
+    }
+}
