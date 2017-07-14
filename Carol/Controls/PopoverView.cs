cf58c9d7386e38a069e19389a24acc8f3faa58ce
AnaghSharma <anaghsharma12@gmail.com>
2017-07-15 00:38:58 +0530

    Added custom class to change background of popover

---
+++ Carol/Controls/PopoverView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using AppKit;
+using Foundation;
+
+namespace Carol.Controls
+{
+    [Register("PopoverView"), DesignTimeVisible(true)]
+    public class PopoverView : NSView
+    {
+        public PopoverView()
+        {
+            
+        }
+
+        public PopoverView(IntPtr p) : base(p)
+        {
+            
+        }
+
+		public override void ViewDidMoveToWindow()
+		{
+			base.ViewDidMoveToWindow();
+
+			var frameView = Window.ContentView.Superview;
+			if (frameView != null)
+			{
+				var backgroundView = new NSView(frameView.Bounds)
+				{
+					WantsLayer = true
+				};
+				backgroundView.Layer.BackgroundColor = NSColor.White.CGColor;
+				backgroundView.AutoresizingMask = (NSViewResizingMask.HeightSizable | NSViewResizingMask.WidthSizable);
+				frameView.AddSubview(backgroundView, NSWindowOrderingMode.Below, frameView);
+			}
+		}
+    }
+}
