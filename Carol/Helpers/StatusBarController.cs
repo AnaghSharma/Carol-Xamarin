3dd7c05c5498afe04723466cbbe73788e30663f0
AnaghSharma <anaghsharma12@gmail.com>
2017-07-14 19:29:06 +0530

    Added StatusBarController helper

---
+++ Carol/Helpers/StatusBarController.cs
@@ -0,0 +1,55 @@
+﻿using AppKit;
+using Foundation;
+
+namespace Carol.Helpers
+{
+    public class StatusBarController : NSObject
+    {
+        readonly NSStatusBar statusBar;
+        readonly NSStatusItem statusItem;
+        readonly NSPopover popover;
+        NSStatusBarButton button;
+
+        public StatusBarController()
+        { 
+            
+        }
+
+        public StatusBarController(string image, NSPopover popover)
+        {
+            //Initialisation
+            statusBar = new NSStatusBar();
+            statusItem = statusBar.CreateStatusItem(NSStatusItemLength.Variable);
+            popover = new NSPopover();
+
+            button = statusItem.Button;
+            button.Image = new NSImage(image)
+            {
+                Template = true
+            };
+            button.Action = new ObjCRuntime.Selector("toggle:");
+            button.Target = this;
+
+            this.popover = popover;
+        }
+
+        [Export("toggle:")]
+        void TogglePopover(NSObject sender)
+        {
+            if (popover.Shown)
+                HidePopover(sender);
+            else ShowPopover(sender);
+        }
+
+        void ShowPopover(NSObject sender)
+        {
+            button = statusItem.Button;
+            popover.Show(button.Bounds, button, NSRectEdge.MaxYEdge);
+        }
+
+        void HidePopover(NSObject sender)
+        {
+            popover.PerformClose(sender);
+        }
+    }
+}
