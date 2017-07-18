1eb53dcd1e72d6bc5655c82801174fe915ceed91
AnaghSharma <anaghsharma12@gmail.com>
2017-07-18 21:53:06 +0530

    Added methods to get api_key

---
+++ Carol/Helpers/SecretsReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Carol.Helpers
+{
+    public class SecretsReader
+    {
+        public string GetSecrets()
+        {
+            JObject secrets = JObject.Parse(GetSecretKey());
+            return secrets["API_key"].Value<String>();
+        }
+
+        public string GetSecretKey()
+        {
+            return File.ReadAllText("Secrets.json");
+        }
+    }
+}
