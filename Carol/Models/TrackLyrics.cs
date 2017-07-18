7cd6a918e8a1f4f9c1de01f87dec661e8000511c
AnaghSharma <anaghsharma12@gmail.com>
2017-07-18 23:15:31 +0530

    Added model for track lyrics

---
+++ Carol/Models/TrackLyrics.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Carol.Models
+{
+    public class TrackLyrics
+    {
+		public class Header
+		{
+			public int status_code { get; set; }
+			public double execute_time { get; set; }
+		}
+
+		public class Lyrics
+		{
+			public int lyrics_id { get; set; }
+			public int can_edit { get; set; }
+			public int locked { get; set; }
+			public string action_requested { get; set; }
+			public int verified { get; set; }
+			public int restricted { get; set; }
+			public int instrumental { get; set; }
+			public int @explicit { get; set; }
+			public string lyrics_body { get; set; }
+			public string lyrics_language { get; set; }
+			public string lyrics_language_description { get; set; }
+			public string script_tracking_url { get; set; }
+			public string pixel_tracking_url { get; set; }
+			public string html_tracking_url { get; set; }
+			public string lyrics_copyright { get; set; }
+			public List<object> writer_list { get; set; }
+			public List<object> publisher_list { get; set; }
+			public string updated_time { get; set; }
+		}
+
+		public class Body
+		{
+			public Lyrics lyrics { get; set; }
+		}
+
+		public class Message
+		{
+			public Header header { get; set; }
+			public Body body { get; set; }
+		}
+
+		public class RootObject
+		{
+			public Message message { get; set; }
+		}
+    }
+}
