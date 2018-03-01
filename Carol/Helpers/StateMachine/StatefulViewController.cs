95dd33f3670d1eca20cce8c6841bdb94abd1a393
AnaghSharma <anaghsharma12@gmail.com>
2018-03-01 23:08:36 +0530

    Initial setup for state machine to handle various ui states

---
+++ Carol/Helpers/StateMachine/StatefulViewController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AppKit;
+
+namespace Carol.Helpers.StateMachine
+{
+    public abstract class StatefulViewController
+    {
+        public NSView ContainerView;
+        public AppDelegate CurrentDelegate = NSApplication.SharedApplication.Delegate as AppDelegate;
+        public abstract void Handle(ViewStateMachine stateMachine);
+    }
+}
---
+++ Carol/Helpers/StateMachine/ViewStateMachine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Carol.Helpers.StateMachine
+{
+    public enum States
+    {
+        Idle,
+        Loading,
+        Content,
+        Error
+    }
+
+    public enum Triggers
+    {
+        Load,
+        ShowContent,
+        ShowEmpty,
+        ThrowError
+    }
+
+    public class ViewStateMachine
+    {
+        public StatefulViewController currentState;
+
+        public ViewStateMachine()
+        {
+        }
+    }
+}
